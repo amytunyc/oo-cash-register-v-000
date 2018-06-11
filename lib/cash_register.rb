43d870a7a8be689fed1b7c01ffa8d415389b6bf5
Amy Tu <siamesebb@gmail.com>
2018-06-11 00:49:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,10 @@ class CashRegister
   end
   
   def apply_discount
-    if discount > 0 
+    if discount = 0
+      puts "There is no discount to apply."
+    else
+      puts 
       
     
 end
